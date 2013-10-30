5f6dda526b373522ce201d506ddfbdbb7bf386de
ala <alluhaia@github.com>
2013-10-29 17:39:42 -0700

    Initialize repository

---
+++ app/models/user.rb
@@ -0,0 +1,29 @@
+class User < ActiveRecord::Base
+    before_save { self.email = email.downcase }
+    before_create :create_remember_token
+   
+    def User.new_remember_token
+        SecureRandom.urlsafe_base64
+    end
+    
+    def User.encrypt(token)
+        Digest::SHA1.hexdigest(token.to_s)
+    end
+    
+    private
+    
+    def create_remember_token
+        self.remember_token = User.encrypt(User.new_remember_token)
+    end
+    
+    def self.validate_login(email, password)
+# //        user = User.find_by_email(email)
+# //        
+# //        if user && user.password==password
+# //        user
+# //        else
+# //        nil
+    end
+
+    
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stu::Application.config.secret_key_base = 'fd6a897d74d5a30f01aba76816335fff9605795532f34a6cd16877626fd6c18f0a1c8ca7acd96dd059d952642b353834d8d670fe2849d52420459cd0e890661c'
